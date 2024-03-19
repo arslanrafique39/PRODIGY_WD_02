2957d78bdc1629ac9d67b722ba47ac1c8f2e64e5
M.Arslan <160122885+arslanrafique39@users.noreply.github.com>
2024-03-20 00:19:16 +0500

    Add files via upload

---
+++ stop watch/ja.js
@@ -0,0 +1,53 @@
+
+let timerdisplay= document.querySelector("#timerdisplay") 
+let startbtn=document.querySelector("#start");
+let pausebtn=document.querySelector("#pause");
+let resetbtn = document.querySelector("#reset");
+
+let msec = 0o0;
+let secs = 0o0;
+let mins = 0o0;
+
+let timerId= null;
+
+startbtn.addEventListener('click' , function()
+{
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId=setInterval(startTimer ,10);
+
+});
+
+pausebtn.addEventListener('click' , function()
+{
+   clearInterval(timerId);
+
+});
+
+resetbtn.addEventListener('click' , function()
+{
+   clearInterval(timerId);
+   timerdisplay.innerHTML=`00 : 00 : 00`;
+    msec = secs =mins= 0o0;
+
+});
+
+
+function startTimer(){
+    msec++;
+    if(msec==100){
+        msec = 0;
+        secs++;
+        if(secs ==60) {
+            secs = 0;
+            mins++;
+    }
+   }
+let msecString = msec <10 ?`0${msec}` : msec;
+let secsString = secs <10 ? `0${secs}` : secs;
+let minsString = mins <10 ? `0${mins}` : mins;
+
+timerdisplay.innerHTML= `${minsString} : ${secsString} : ${msecString}`;
+
+}
